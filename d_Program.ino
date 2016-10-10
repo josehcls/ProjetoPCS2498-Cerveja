a178e15c1c9bfdf9f79d8ec2f7a488ca76dc5f03
Gabi <gabimelo10@gmail.com>
2016-10-10 10:41:34 -0300

    Finalizada funcao stand by

--- d_Program.ino
+++ d_Program.ino
@@ -20,15 +20,9 @@ void standBy(){
 	lcd.setCursor(0,1);
     lcd.print("Inicie no app!");
 
-    if (brewStage == 0 && switchState == HIGH) {
-    	brewStage = 1;
-
-    	terminal.println("PREPARING STRIKE WATER. STIR EVERY FEW MINUTES");
+    if () { // ver quando recebe aperto de botao no app
+    	terminal.println("Iniciando estágio de aquecimento de água");
     	terminal.flush();
-    	Blynk.notify(String("Brewbot message. Heating strike water"));
-
-    	buzzer = HIGH;
-    	buzzer = LOW;
     }
 }
 
@@ -37,7 +31,30 @@ void heatTilMashTemp(){
 }
 
 void mashing(){
-	
+	lcd.setCursor(0,0);
+	lcd.print("Agua quente, ver");
+	lcd.setCursor(0,1);
+    lcd.print("inst. no app");
+
+	if ( immersionTemp < mashTemp - 2) {
+		heater1 = HIGH;
+	}
+
+	else if ( immersionTemp > mashTemp + 2 ) {
+		heater1 = LOW;
+	}
+
+	if (brewStage == 2 && switchState == HIGH) {
+		brewStage = 3;
+		terminal.println("THE MASH STARTS NOW. STIR EVERY FEW MINUTES TO ENSURE EVEN TEMPERATURE DISTRIBUTION. MAKE SURE NO GRAINS ARE CLOGGED TOGETHER ");
+
+		terminal.flush();
+		minutesCounter = 0;
+		//timeEstimate = 0;
+
+		buzzer = HIGH;
+		buzzer = LOW;
+	}
 }
 
 void heatTilBoil(){
